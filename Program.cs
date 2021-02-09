59b077ef4910ad4179ffdc9325c62cb7995c8927
AzureAD\YoavCohenLax <yoav@capito.co.il>
2021-02-09 09:56:02 +0200

    CommitFromfeature

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace Demo1
             Console.WriteLine("Iteration number: " + i);
             i=100;
             Console.WriteLine(i++);
+            Console.WriteLine("Welcome");
         }
     }
 }
