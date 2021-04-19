930a91ccfb240dadeacde2a02964280874a89213
AzureAD\YoavCohenLax <yoav@capito.co.il>
2021-04-19 12:19:11 +0300

    change 2

--- Person.cs
+++ Person.cs
@@ -14,6 +14,6 @@ namespace Demo1
         string ID;
         bool IsTrue;
         Guid Identifier;
-        // Hii from Release1
+        // Hii from Release1 - another change from release 1
     }
 }
